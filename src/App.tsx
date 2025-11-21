93fa182d6b367f308981a52d0e6f22002b55d3a0
Red.G <red.g@example.com>
2025-11-21 20:28:36 +0100

    Upgrade Spline to fix scene loading

--- src/App.tsx
+++ src/App.tsx
@@ -1137,11 +1137,7 @@ function SplinePage() {
       {/* Spline plein écran */}
       <Spline
         scene="https://prod.spline.design/eUR0ZkHlU2oliRLX/scene.splinecode"
-        onLoad={handleSplineLoad}
-        onError={(error) => {
-          console.error("🚨 Spline failed to load:", error);
-          setSplineLoaded(true); // Continuer même si Spline fail
-        }}
+        onLoad={onLoad}
         renderOnDemand={false}
         style={{
           width: "100vw",
